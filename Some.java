2e64a4c58a138c0d4223caf6b251b655d341148e
Yuvakishore9 <kishorepushadapu99@gmail.com>
2023-06-28 07:34:02 +0200

    Day-2 Files

---
+++ Some.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Some{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any 2 numbers:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("The sum is:"+(a+b));
+    }
+}
